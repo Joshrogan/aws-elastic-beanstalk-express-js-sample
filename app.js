f39d20e9ce11cdf77d1d4c597625ed7a2fe023f7
Joshua Rogan <joshua.rogan@mydit.ie>
2020-12-17 12:44:18 +0000

    testing message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Dummy Pipeline! try again! manual review. Testing webhook now flkdjfdflkj'));
+app.get('/', (req, res) => res.send('After you commit, the pipeline starts and will require manual approval before final stage'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
