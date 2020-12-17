35b2703d45bdb627b3e1b1006bd9ccde8799c113
Joshua Rogan <joshua.rogan@mydit.ie>
2020-12-17 13:04:32 +0000

    commit test pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('After you commit, the pipeline starts and will require manual approval before final stage'));
+app.get('/', (req, res) => res.send('After you commit, the pipeline starts and will require manual approval before final stage added this'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
